5dccde143f7fe3ae1b9294fb22c0d0201dec1a0a
Daniel Hartnell <dhartnell@newrelic.com>
2015-10-22 20:29:45 -0700

    refactored and wrote a new failing test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
 var isVowel = function(input) {
-
   return input.concat("ay");
 };
 
 var isConsonant = function(input) {
-  var firstCharacter = input.slice(0,1);
-  return input.slice(1,input.length).concat(firstCharacter).concat("ay");
+  var firstChars = input.slice(0,1);
+  var length = input.length;
+
+  return input.slice(1,length).concat(firstChars).concat("ay");
 };
 
 $(document).ready(function() {
-  $("form#english-input").submit(function(event) {
+  $("form").submit(function(event) {
     var input = $("#input").val();
-    var firstCharacter = input.slice(0,1);
-
-    if (firstCharacter.toLowerCase() === "a" || "e" || "i" || "o" || "u") {
+    if (input.toLowerCase().match(/^(a|e|i|o|u)/)) {
+      $(".translation").text("");
       $(".translation").text(isVowel(input));
       $("#result").show();
-    }
-
-    if (firstCharacter.toLowerCase() === "b" || "c" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "m" || "n" || "p" || "q" || "r" || "s" || "t" || "v" || "w" || "x" || "y" || "z") {
+    } else {
+      $(".translation").text("");
       $(".translation").text(isConsonant(input));
       $("#result").show();
     }
--- spec/specs.js
+++ spec/specs.js
@@ -8,4 +8,8 @@ describe('isConsonant', function() {
   it("perform appropriate pig latin translation for words that start with a consonant", function() {
     expect(isConsonant("mouse")).to.equal("ousemay");
   });
-})
+
+  it("works when a word starts with more than one consonant", function() {
+    expect(isConsonant("Translate")).to.equal("anslateTray");
+  });
+});
